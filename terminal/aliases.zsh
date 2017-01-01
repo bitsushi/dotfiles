d62abf6c7977c5f29581e2add471207270810b8e
John Rees <john@bitsushi.com>
2017-01-01 13:14:41 +0000

    terminal stuff

---
+++ terminal/aliases.zsh
@@ -0,0 +1,12 @@
+# Increase Bash history size. Allow 32³ entries; the default is 500.
+export HISTSIZE='32768';
+export HISTFILESIZE="${HISTSIZE}";
+
+# Omit duplicates and commands that begin with a space from history.
+export HISTCONTROL='ignoreboth';
+
+# Highlight section titles in manual pages.
+export LESS_TERMCAP_md="${yellow}";
+
+# Don’t clear the screen after quitting a manual page.
+export MANPAGER='less -X';
